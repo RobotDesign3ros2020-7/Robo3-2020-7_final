524cf6056dca5d403249be6755fcf3864dd28470
yuya <s19c1051vs@s.chibakoudai.jp>
2020-12-14 14:31:05 +0900

    add

--- crane_x7_examples/scripts/dousa1.py
+++ crane_x7_examples/scripts/dousa1.py
@@ -10,7 +10,7 @@ from tf.transformations import quaternion_from_euler
 
 def main():
     rospy.init_node("pose_groupstate_example")
-    pub = rospy.Publisher("image", Int32, queue_size=1)
+    pub = rospy.Publisher("image", quaternion_from_euler, queue_size=1)
     robot = moveit_commander.RobotCommander()
     arm = moveit_commander.MoveGroupCommander("arm")
     arm.set_max_velocity_scaling_factor(0.75)
