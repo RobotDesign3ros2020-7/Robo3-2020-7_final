9978c415995142eadd4bac9451166685826c07db
HoshinoMasafumi <s19C1105lW@s.chibakoudai.jp>
2020-12-21 03:24:26 +0900

    add

--- crane_x7_examples/scripts/vision.py
+++ crane_x7_examples/scripts/vision.py
@@ -14,7 +14,6 @@ class Follower:
         self.twist = Twist()
 
     def image_callback(self, msg):
-        #image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         image = self.bridge.imgmsg_to_cv2(msg)
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -22,7 +21,6 @@ class Follower:
         upper_red = numpy.array([185,255,255])
         mask = cv2.inRange(hsv, lower_red, upper_red)
 
-        #masked = cv2.bitwise_and(image, image, mask=mask)
 
         M = cv2.moments(mask)
         if M['m00'] > 0:
@@ -31,15 +29,11 @@ class Follower:
             cv2.circle(image, (cx, cy), 5, (0,0,125), -1)
             print(cx,cy)
 
-            #msg.cx = cx
-            #msg.cy = cy
-
             pub1.publish(cx)
             pub2.publish(cy)
 
         cv2.imshow("window", mask)
         cv2.imshow("image", image)
-        #cv2.imshow("window1", hsv)
         cv2.waitKey(3)
 
 rospy.init_node('follower')
